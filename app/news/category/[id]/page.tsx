aaf7cd234736cceb8e2d859a9cf2786658a121ba
haruka-0214 <ktc24a31e0015@edu.kyoto-tech.ac.jp>
2025-01-25 16:30:30 +0900

    commit

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { getCategoryDeteil,getNewsList } from "@/app/_libs/microcms";
+import {notFound} from "next/navigation";
+import NewsList from "@/app/_components/NewsList";
+import Category from "@/app/_components/Category";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+
+type Props ={
+    params:{
+        id:string;
+    };
+};
+
+export default async function page({params}:Props) {
+    const category=await getCategoryDeteil(params.id).catch(notFound);
+
+    const {contents:news}=await getNewsList({
+        limit:NEWS_LIST_LIMIT,
+        filters:`category[equals]${category.id}`,
+    });
+
+    return (
+        <>
+            <p>
+                <Category category={category} />の一覧
+            </p>
+            <NewsList news={news} />
+        </>
+    );
+}
